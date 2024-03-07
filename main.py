c86e3b63c33ee011a9114f5b55c14f38d3681814
Prathyarthi <prathyarti@gmail.com>
2024-03-07 16:18:08 +0530

    Prompt and chain

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 import google.generativeai as genai
 
@@ -32,5 +33,21 @@ def vector_store(text_chunks):
     vector_store = FAISS.from_texts(text_chunks, embedding = embeddings)
     vector_store.save_local("faiss_index")
 
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
 
